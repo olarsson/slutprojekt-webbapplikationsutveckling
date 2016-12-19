e412555afa5bf50096c14462802ffbdc616a9f2d
folofolo <olarsson@live.com>
2016-12-19 20:45:02 +0100

    Error improvements

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-
 "use strict";
 
 function parse_json(data) {
   return jQuery.parseJSON(data);
+}
+
+function print_html(container, data, error) {  
+  if (error) data = '<div class="col-md-12 col-sm-12 col-xs-12">Ett fel uppstod: '+data+'</div>';
+  container.find('.ajax_content').first().html('<div class="row">' + data + '</div>');
+  container.find('.ajax_loading').first().css('display', 'none');
 }
 
 function send_req(container, mode, req_url, req_type) {
@@ -17,42 +22,38 @@ function send_req(container, mode, req_url, req_type) {
     dataType: req_type,
     type: "GET",
     cache: false,
-    timeout: 10000,
+    timeout: 1000,
 
     complete: function (jqXHR, textStatus) {
       
       //"success", "notmodified", "nocontent", "error", "timeout", "abort", or "parsererror"
       if (textStatus == 'success') {
         
-        //query: coolt
-        if (mode == 0 && (data = parse_json(jqXHR.responseText))){
-          
-          container.find('.ajax_loading').first().css('display', (jqXHR.responseText ? 'none' : 'block'));
-          html += `<div class="row">`;
+        data = parse_json(jqXHR.responseText);
+        
+        if (mode == 0 && data) {
           
           var pdate;
-                        
+
           for (var i in data.items) {
-            
-            pdate = ((pdate = data.items[i].volumeInfo.publishedDate) ? pdate : 'Okänt');
-            
+            pdate = ((pdate = data.items[i].volumeInfo.publishedDate) ? pdate : 'Okänt datum');
             html += `<div class="col-md-2 col-sm-4 col-xs-12"><div class="img_hover">`;
             html += (data.items[i].volumeInfo.hasOwnProperty("imageLinks") ? `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}">` : `<img src="img/thumbnail.png">`);
-            html += `<div class="titledata">${data.items[i].volumeInfo.title}</div>`;            
-            html += `</div><span class="date text-muted">${pdate}</span></div>`;
-          }                        
+            html += `<div class="titledata">${data.items[i].volumeInfo.title}<div class="date">${pdate}</div></div>`;            
+            html += `</div></div>`;
+          }
           
-          html += `</div>`;
-          container.find('.ajax_content').first().html(html);          
-          
-          //console.log(html);
-          //console.info(data.items);
+          print_html(container,  (data.totalItems == 0 ? 'Inga resultat hittades' : html)  , (data.totalItems == 0 ? true : false));
 
+        } else {
+          //no more modes, throw error
         }
 
-        
       } else {
-        console.info(jqXHR);
+        
+        //request failed, timed out, etc..
+        print_html(container, textStatus, true);
+        
       }
       
     }
